343669d986e74b7659eed7ba8742562bb057fc04
Cleverson-Borges <brgscleverson.pro@gmail.com>
2024-11-11 21:57:18 -0300

    verificacao de 24h

--- desk_manager/routes/registros.py
+++ desk_manager/routes/registros.py
@@ -1,8 +1,8 @@
 from desk_manager.models import PlanoDeUso, EstadoReserva
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from desk_manager.extensions import db
-from desk_manager.routes.reserva import Reserva, RESERVA
-from datetime import datetime
+from desk_manager.routes.reserva import Reserva, RESERVA, buscar_reserva_por_codigo
+from datetime import datetime, timedelta
 from sqlalchemy import func
 import uuid
 
@@ -13,7 +13,7 @@ def listagem():
     hoje = datetime.now().date()
     reservas = Reserva.query.filter(func.date(Reserva.data) == hoje).all()
     reservas_dict = [reserva.to_dict() for reserva in reservas]
-    flash(f"Listando reservas para o dia {hoje.strftime("%d/%m/%Y")}", "success")
+    #flash(f"Listando reservas para o dia {hoje.strftime("%d/%m/%Y")}", "success")
     return render_template('listagem_registros.html', reservas=reservas_dict)
 
 
@@ -47,15 +47,19 @@ def registrar_saida(reserva_id):
 @REGISTROS.route('/reservas/<string:reserva_id>/registrar_cancelamento', methods=['POST'])
 def registrar_cancelamento(reserva_id):
     reserva = Reserva.query.get_or_404(reserva_id)
+    hoje = datetime.now()
+
     if reserva.estado == EstadoReserva.RESERVADA:
-        reserva.data_cancelamento = datetime.now()
-        reserva.estado = EstadoReserva.CANCELADA
-        db.session.commit()
-        flash("Cancelamento registrado com sucesso!", "warning")
-        return redirect(url_for('registros.listagem', reserva_id=reserva.id))
+        if reserva.data > hoje + timedelta(days=1):
+            reserva.data_cancelamento = datetime.now()
+            reserva.estado = EstadoReserva.CANCELADA
+            db.session.commit()
+            flash("Cancelamento registrado com sucesso!", "warning")
+        else:
+            flash("O cancelamento só pode ser feito até 24 horas antes da data da reserva.", "warning")
     else:
         flash("Só pode se cancelar uma reserva que ainda não foi iniciada", "warning")
-        return  redirect(url_for('registros.listagem', reserva_id=reserva.id))
+    return  redirect(url_for('registros.listagem', reserva_id=reserva.id))
 
 @REGISTROS.route('/buscar_listagem/<string:codigo>', methods=['GET'])
 def buscar_listagem_por_codigo(codigo):
